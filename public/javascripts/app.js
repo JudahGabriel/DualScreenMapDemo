309fc540d634052762b3f27c433c2492d126ae21
Judah Gabriel Himango <judahgabriel@gmail.com>
2020-03-12 17:10:14 -0700

    Removed beforeunload listener

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -141,13 +141,3 @@ const products = [
       btn.addEventListener("click", () => btn.classList.toggle("liked"));
     }
   );
-
-  window.addEventListener("beforeunload", event => {
-    if (event.dialog) {
-      event.dialog.setMessage("You have unsaved stuff!");
-      event.dialog.setButtonLabel("Save the stuff");
-      event.dialog.show().then(async (result) => {
-        // save the stuff
-      })
-    }
-  });
